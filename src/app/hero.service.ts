dc12eab7ab12c99cec21ceb0e543eb44526b7c6c
Antares-8 <flo-qumal@hotmail.fr>
2020-02-27 14:17:01 +0100

    add messages for users

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // Send message when heroes are fetched
+    this.messageService.add('HeroService says : fetched heroes');
+
     return of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
   }
 }
