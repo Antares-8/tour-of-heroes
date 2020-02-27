aecf74106e0e47977a0e937a0093169c976bae4d
Antares-8 <flo-qumal@hotmail.fr>
2020-02-27 11:29:43 +0100

    show heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Spiderman' },
+  { id: 3, name: 'Captain America' },
+  { id: 4, name: 'Black Widow' },
+  { id: 5, name: 'Black Panther' },
+  { id: 6, name: 'Ant Man' },
+  { id: 7, name: 'Hulk' },
+  { id: 8, name: 'Thor' },
+  { id: 9, name: 'Dead Pool' },
+  { id: 10, name: 'Dr Strange' }
+];
