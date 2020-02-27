5f2fbc00d073f3e34c7a2fe2e5da916c6f83023c
Antares-8 <flo-qumal@hotmail.fr>
2020-02-27 17:12:20 +0100

    post hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,16 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Iron Man' },
+      { id: 2, name: 'Spiderman' },
+      { id: 3, name: 'Captain America' },
+      { id: 4, name: 'Black Widow' },
+      { id: 5, name: 'Black Panther' },
+      { id: 6, name: 'Ant Man' },
+      { id: 7, name: 'Hulk' },
+      { id: 8, name: 'Thor' },
+      { id: 9, name: 'Dead Pool' },
+      { id: 10, name: 'Dr Strange' }
     ];
     return {heroes};
   }
