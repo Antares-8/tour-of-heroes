69e25ac9ccf159fb12b5ca7f122c683192bd86cc
Antares-8 <flo-qumal@hotmail.fr>
2020-02-27 16:59:52 +0100

    heroes with observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -23,7 +23,9 @@ export class HeroService {
     //return of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
     /** GET heroes from the server */
     return this.http.get<Hero[]>(this.heroesUrl) // get() returns response data
-    .pipe(catchError(this.handleError<Hero[]>('getHeroes', []))
+    .pipe(
+      tap(_ => this.log('fetched heroes')), // tap looks, does smthg and send observable values
+      catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
 
@@ -47,10 +49,10 @@ export class HeroService {
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
+      // send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
+      // better job of transforming error for user consumption
       this.log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
