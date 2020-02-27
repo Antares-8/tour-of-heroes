fb5b8306f9c1e3699cf72a3920da519cc897e36e
Antares-8 <flo-qumal@hotmail.fr>
2020-02-27 16:49:12 +0100

    heroes in http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
@@ -9,13 +10,18 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+  private http: HttpClient,
+  private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // Send message when heroes are fetched
     this.messageService.add('HeroService says : fetched heroes');
-
-    return of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+    //return of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+    /** GET heroes from the server */
+    return this.http.get<Hero[]>(this.heroesUrl); // get() returns response data
   }
 
   getHero(id: number): Observable<Hero> {
@@ -23,5 +29,9 @@ export class HeroService {
     this.messageService.add(`HeroService says : fetched hero id  = ${id}`); // /!\ backtick to bind property (id here)
 
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService says : ${message}`);
   }
 }
